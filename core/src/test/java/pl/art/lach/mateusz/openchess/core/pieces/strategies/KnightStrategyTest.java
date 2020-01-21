59837b0397edf35746254e06c9556bd6796a2ac1
Mateusz Lach <matis_l@tlen.pl>
2020-01-21 19:56:06 +0100

    added more tests for knight

--- core/src/test/java/pl/art/lach/mateusz/openchess/core/pieces/strategies/KnightStrategyTest.java
+++ core/src/test/java/pl/art/lach/mateusz/openchess/core/pieces/strategies/KnightStrategyTest.java
@@ -27,34 +27,69 @@ import pl.art.lach.mateusz.openchess.core.board.Field.Letter;
 import pl.art.lach.mateusz.openchess.core.board.Field.Number;
 
 /**
- * @author: Mateusz Sławomir Lach 
+ * @author: Mateusz Sławomir Lach
  */
 public class KnightStrategyTest {
-    
+
     private KnightStrategy knightStrategy = new KnightStrategy();
 
     @Test
     public void knightFieldsInRangeTest_C4() {
         Field fieldC4 = Field.getInstance(Letter._C, Number._4);
         final Set<Field> fields = knightStrategy.getAllFieldsInRange(fieldC4, Color.WHITE);
-        
+
         Field fieldA3 = Field.getInstance(Letter._A, Number._3);
         Field fieldA5 = Field.getInstance(Letter._A, Number._5);
-        
+
         Field fieldB2 = Field.getInstance(Letter._B, Number._2);
         Field fieldB6 = Field.getInstance(Letter._B, Number._6);
-        
+
         Field fieldD2 = Field.getInstance(Letter._D, Number._2);
         Field fieldD6 = Field.getInstance(Letter._D, Number._6);
-        
+
         Field fieldE3 = Field.getInstance(Letter._E, Number._3);
         Field fieldE5 = Field.getInstance(Letter._E, Number._5);
-        
+
         assertThat(fields, hasItems(fieldA3, fieldA5));
         assertThat(fields, hasItems(fieldB2, fieldB6));
         assertThat(fields, hasItems(fieldD2, fieldD6));
         assertThat(fields, hasItems(fieldE3, fieldE5));
-        
-     }
 
+    }
+
+    @Test
+    public void knightFieldsInRangeTest_A1() {
+        Field fieldA1 = Field.getInstance(Letter._A, Number._1);
+        final Set<Field> fields = knightStrategy.getAllFieldsInRange(fieldA1, Color.WHITE);
+
+        Field fieldC2 = Field.getInstance(Letter._C, Number._2);
+        Field fieldB3 = Field.getInstance(Letter._B, Number._3);
+
+        assertThat(fields, hasItems(fieldC2, fieldB3));
+
+    }
+    
+    @Test
+    public void knightFieldsInRangeTest_H8() {
+        Field fieldH8 = Field.getInstance(Letter._H, Number._8);
+        final Set<Field> fields = knightStrategy.getAllFieldsInRange(fieldH8, Color.WHITE);
+        
+        Field fieldF7 = Field.getInstance(Letter._F, Number._7);
+        Field fieldG6 = Field.getInstance(Letter._G, Number._6);
+        
+        assertThat(fields, hasItems(fieldF7, fieldG6));
+        
+    }
+    
+    @Test
+    public void knightFieldsInRangeTest_A8() {
+        Field fieldA8 = Field.getInstance(Letter._A, Number._8);
+        final Set<Field> fields = knightStrategy.getAllFieldsInRange(fieldA8, Color.WHITE);
+        
+        Field fieldB6 = Field.getInstance(Letter._B, Number._6);
+        Field fieldC7 = Field.getInstance(Letter._C, Number._7);
+        
+        assertThat(fields, hasItems(fieldB6, fieldC7));
+        
+    }
 }
